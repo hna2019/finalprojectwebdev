2cb74f95588474fde004012403c3476c78685afa
kaidoemling <kai.doemling@gmail.com>
2025-12-05 22:33:15 -0500

    Media Query/ShoppingCart

---
+++ Final/script.js
@@ -0,0 +1,25 @@
+let svg = document.querySelector(".svg");
+let nav = document.querySelector("#navigation");
+let isOpen = false;
+
+svg.addEventListener("click", menu);
+
+    
+function menu(){
+    
+    if (isOpen) {
+        isOpen = false;
+        nav.style.display = "none";
+    } else {
+        isOpen = true;
+        nav.style.display = "flex";
+    }
+                
+}
+
+window.addEventListener('resize', function() {
+    if (window.innerWidth > 768) {
+        nav.style.display = ''; 
+        isOpen = false;
+    }
+});
